33b5c7c17e5a5b294dc0dfdf812b46a343eeae8f
emmramon15 <60593785+emmramon15@users.noreply.github.com>
2020-03-04 15:13:57 -0600

    Midterm
    code for midterm

---
+++ MidtermEmmaleeRamon.appstudio/forms/incomeBracket/incomeBracket.js
@@ -0,0 +1,16 @@
+btnSubmit.onclick=function(){
+  let income= inpt.Income.value
+  let bracket= "With your income of $ ${income}  you are in a tax bracket of  "
+    if (income < 30000) {
+    lblBracket.value = bracket + '8%'
+  } else if (income < 99999 && income >= 30000) {
+    lblBracket.value = bracket + '15%'
+  } else if (income >= 99999) {
+    lblBracket.value = bracket + '25%' 
+    }
+}
+
+
+btnGoAgain.onclick=function(){
+  lblBracket.value = "Enter a new income"
+}
